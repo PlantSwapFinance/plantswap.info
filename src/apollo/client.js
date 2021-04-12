67d3e8c7ae085095e5fd5917b5a74d3462277973
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 22:25:29 -0400

    Change thegraph url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/plantswap/exchange',
+    uri: 'https://api.thegraph.com/pancakeswap/name/plantswap/exchange',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -20,7 +20,7 @@ export const healthClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/plantswap/blocks',
+    uri: 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks',
   }),
   cache: new InMemoryCache(),
 })
