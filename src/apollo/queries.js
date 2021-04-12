03165b4998d9525812453bb782412e409c9370a2
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 22:31:11 -0400

    change graph path

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "plantswap/exchange") {
+    indexingStatusForCurrentVersion(subgraphName: "pancakeswap/exchange") {
       synced
       health
       chains {
