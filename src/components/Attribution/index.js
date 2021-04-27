0c670931e9c6fe816daff5f8b62419c245f3e0f4
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-27 18:27:25 -0400

    change to plantswap

--- src/components/Attribution/index.js
+++ src/components/Attribution/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Attribution = () => (
   <p className="attribution">
-    <a href="https://github.com/pancakeswap/pancakeswap-info" rel="noopener noreferrer" target="_blank">
+    <a href="https://github.com/plantswapfinance/plantswap-info" rel="noopener noreferrer" target="_blank">
       Github
     </a>{' '}
     |{' '}
-    <a href="https://pancakeswap.io" rel="noopener noreferrer" target="_blank">
-      pancakeswap
+    <a href="https://plantswap.finance" rel="noopener noreferrer" target="_blank">
+    plantswap
     </a>{' '}
     |{' '}
     <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import { Flex } from 'rebass'
 import Link from '../Link'
 
 const links = [
-  { url: 'https://pancakeswap.finance', text: 'About' },
-  { url: 'https://docs.pancakeswap.finance', text: 'Docs' },
-  { url: 'https://github.com/pancakeswap/pancakeswap-info', text: 'Code' },
+  { url: 'https://plantswap.finance', text: 'About' },
+ // { url: 'https://docs.pancakeswap.finance', text: 'Docs' },
+  { url: 'https://github.com/plantswapfinance/plantswap-info', text: 'Code' },
 ]
 
 const FooterLink = ({ children, ...rest }) => (
